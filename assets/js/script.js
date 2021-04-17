117f8c4d1f12462c281587b4e4c5b05b5e8d999f
Laura <lauraspberrypie@gmail.com>
2021-04-17 12:48:34 -0700

    rotating text

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,28 @@ function changeTheme() {
     document.body.className = `theme-${theme}`;
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('#timelineModal1');
-    var instances = M.Modal.init(elems, options);
-});
+// source: https://stackoverflow.com/questions/44169668/how-to-loop-changing-text-in-html-javascript
+var introid = ['a', 'an', 'a', 'a', 'an', 'a'];
+var identity = ['programmer', 'artist', 'problem solver', 'minecraft enthusiast', 'egg tart fanatic', 'student'];
+
+textSequence(0);
+function textSequence(i) {
+
+    if (identity.length > i) {
+        setTimeout(function() {
+            document.getElementById("sequenceA").innerHTML = introid[i];
+            document.getElementById("sequence").innerHTML = identity[i];
+            textSequence(++i);
+        }, 2000); // 2 seconds (in milliseconds)
+
+    } else if (identity.length == i) { // Loop
+        textSequence(0);
+    }
+
+}
+
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('#timelineModal1');
+//     var instances = M.Modal.init(elems, options);
+// });
