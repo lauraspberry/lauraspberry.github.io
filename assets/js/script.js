08b3ddde224deb87ddf0ff26142a191779ea3c0b
Laura <lauraspberrypie@gmail.com>
2021-04-24 22:27:03 -0700

    attempt to type text but failed so removed anim

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,8 +45,19 @@ function textSequence(i) {
 
     if (identity.length > i) {
         setTimeout(function() {
+            // css fade out - class for hiding / fading
             document.getElementById("sequenceA").innerHTML = introid[i];
+            let currWord = identity[i];
+            // for (let l = 0; l < currWord.length + 1; l++) {
+            //     let tempString = currWord.substring(0, l);
+            //     sleep(100);
+            //     console.log(tempString);
+            //     document.getElementById("sequence").innerHTML = tempString;
+                
+            //     // wait 
+            // }
             document.getElementById("sequence").innerHTML = identity[i];
+            // add css class
             textSequence(++i);
         }, 2000); // 2 seconds (in milliseconds)
 
@@ -61,3 +72,33 @@ function textSequence(i) {
 //     var elems = document.querySelectorAll('#timelineModal1');
 //     var instances = M.Modal.init(elems, options);
 // });
+
+let e = document.getElementsByClassName("lightbox");
+
+function closeModal() {
+    e.classList.add("hidden");
+}
+
+e.onclick = closeModal;
+
+
+function sleep(milliseconds) {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+      currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
+  }
+
+  function type() {
+    document.getElementById("sequenceA").innerHTML = introid[i];
+    let currWord = identity[i];
+    for (let l = 0; l < currWord.length + 1; l++) {
+        let tempString = currWord.substring(0, l);
+        sleep(100);
+        console.log(tempString);
+        document.getElementById("sequence").innerHTML = tempString;
+        
+        // wait 
+    }
+  }
